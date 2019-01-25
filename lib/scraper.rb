16f10701eb4a8a6c8e053dc7be8afeeb8548336f
Laura Felce <crepu83@yahoo.it>
2019-01-25 09:01:57 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,35 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    
-  end
+
+    def self.scrape_index_page(index_url)
+      html = File.read("./fixtures/student-site/index.html")
+      doc = Nokogiri::HTML(html)
+      students = []
+      doc.css("div.student-card").each do |stu_card|
+      student_cards = {}
+      student_cards[:name] = stu_card.css("h4.student-name").text
+      student_cards[:location] = stu_card.css("p.student-location").text
+      student_cards[:profile_url] = stu_card.css("a").attribute("href").value
+      students << student_cards
+      end
+      students
+    end
+
+
 
   def self.scrape_profile_page(profile_url)
-    
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+    profile = {}
+    profile[:twitter] = doc.css.("a").first.css.attribute("href").value
+    profile[:profile_quote] = doc.css("div.profile-quote").text
+    profile[:bio] = doc.css("div.description-holder p").text
+    profile
+    binding.pry
   end
 
 end
-
